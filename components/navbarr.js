20113ef205458bf188bb46e183bcd4a7240e8ff9
Vipul <Vipulsoniab17@gmail.com>
2021-08-08 15:03:28 +0530

    add index.htmk

---
+++ components/navbarr.js
@@ -0,0 +1,14 @@
+function navbar() {
+  return ` <div id="container">
+      <input placeholder="Search Dish"  type="text" id="query" />
+
+      <div class="options">
+        <h3><a id="text1" href="receipeoftheday.html">Receipe Of The Day</a></h3>
+        <h3 id="hello"><a id="text2" href="LatestReceipe.html">Latest Receipe</a></h3>
+         <h3 id="hello"><a id="text2" href="index.html">Home</a></h3>
+      </div>
+    </div>
+    <div id="data"></data>   `;
+}
+
+export default navbar;
